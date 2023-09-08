0b91f27ed3dfcaf0ce1745f0a162cf96028358f6
menej <68961385+menej@users.noreply.github.com>
2023-09-08 14:09:05 +0200

    feat: Add updating digits, clearing and updating display functionality
    Added event listeners to buttons for clearing, updating digits on the
    display and the operator buttons to call operate function. Added initial
    values for the three important global variables and implemented function
    to update digits.

---
+++ script.js
@@ -0,0 +1,63 @@
+function updateDisplay(number) {
+    let display = document.querySelector(".display");
+    display.textContent = number;
+}
+
+function updateDigit(e) {
+    let number = e.target.getAttribute('data-key');
+    if (op === null) {
+        // Display can display up to 10 digits
+        if (a.length === 10) return;
+
+        if (a === "0") a = number;
+        else a += number;
+
+        updateDisplay(a);
+    } else {
+        // Display can display up to 10 digits
+        if (b.length === 10) return;
+
+        if (b === "0") b = number;
+        else b += number;
+        updateDisplay(b);
+    }
+}
+
+function operate(e) {
+    let operation = e.target.getAttribute("data-key");
+
+    // Check if operation is equals and the current operation is null
+    if (operation === "Enter" && op === null) return;
+
+    // Check if operation is already declared
+    if (op === null) {
+        op = operation;
+        return;
+    }
+}
+
+function clear(e) {
+    a = "0";
+    b = "0";
+    op = null;
+    updateDisplay(a);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let clearButton = document.querySelector('button[data-key="Backspace"]');
+    clearButton.addEventListener("click", clear);
+
+    let digitButtons = document.querySelectorAll('.digit');
+
+
+    digitButtons.forEach((button) => button.addEventListener("click", updateDigit));
+
+    let operatorButtons = document.querySelectorAll('.operator');
+    operatorButtons.forEach((button) => button.addEventListener("click", operate))
+});
+
+
+let a = "0";
+let b = "0";
+let op = null;
+updateDisplay(a);  // Initiate the display
